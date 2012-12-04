9d24877b336d44d304176e3925024e01f4f6a98e
Tom Rees <zephod@gmail.com>
2012-12-04 13:07:44 +0000

    Refactoring and optimising

--- iatilib/__init__.py
+++ iatilib/__init__.py
@@ -1,6 +1,8 @@
 import os
 import sqlalchemy
 import sqlalchemy.orm
+from datetime import datetime
+import traceback
 
 database_url = os.environ.get('DATABASE_URL')
 database_echo = os.environ.get('DATABASE_ECHO', '').lower()=='true'
@@ -12,5 +14,22 @@ engine = sqlalchemy.create_engine(database_url,echo=database_echo)
 Session = sqlalchemy.orm.scoped_session(sqlalchemy.orm.sessionmaker(engine))
 
 import model
-import importer
+from importer import Importer
+
+# Simple logfile class. Use a library if this gets any more than ~15 lines
+class LogFile:
+    def __init__(self,filename,log_to_screen=True):
+        self.file = open(filename,'a')
+        self.log_to_screen = log_to_screen
+    def write(self,text):
+        if self.log_to_screen:
+            print text
+        text = '%s %s' % (str(datetime.now().strftime("%Y-%m-%d %H:%M:%S")), text)
+        print >>self.file, text
+    def write_traceback(self):
+        if self.log_to_screen:
+            traceback.print_exc()
+        traceback.print_exc(file=self.file)
+    def close(self):
+        self.file.close()
 
--- job_import.py
+++ job_import.py
@@ -1,12 +1,32 @@
 import iatilib
+import argparse
+import sys
+import os
+import traceback
+from datetime import date
 
-if __name__ == '__main__':
-    import sys
+def main(data_dump):
+    assert os.path.exists(data_dump), 'Data dump folder "%s" does not exist.' % data_dump
+    logfilename = 'log-%s.txt' % str(date.today())
+    log = iatilib.LogFile( logfilename )
+    log.write('Commencing import task. Directory=%s' % data_dump)
     try:
-        iatilib.importer.load_package()
-    except Exception, e:
-        print 'Failed:', e
-        logtext = "Couldn't load package: " + str(e) + "\n"
-        log(logtext)
-    print iatilib.Session.query(iatilib.model.Activity).count()
-    print iatilib.Session.query(iatilib.model.Transaction).count()
+        importer = iatilib.Importer(log)
+        importer.load_directory(data_dump)
+        count_activity = iatilib.Session.query(iatilib.model.Activity).count()
+        count_transaction = iatilib.Session.query(iatilib.model.Transaction).count()
+        log.write('Completed import task. Database contains:')
+        log.write('  %d activities' % count_activity)
+        log.write('  %d transactions' % count_transaction)
+    except Exception as e:
+        traceback.print_exc()
+        log.write('Import task failed. Exception')
+        log.write(str(e))
+    log.close()
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser(description='Parse an IATI data dump folder and populate the database with activities.')
+    parser.add_argument('data_dump', help='Data dump folder to parse.')
+    #parser.add_argument('-v', '--verbose', action='store_true', dest='verbose', help='Verbose output')
+    arg = parser.parse_args()
+    main(arg.data_dump)
