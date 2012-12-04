1103dbcc043fcaac54f778eebe6d80ce01185262
Tom Rees <zephod@gmail.com>
2012-12-04 22:11:16 +0000

    Frontend spits out DB content

--- frontend.py
+++ frontend.py
@@ -1,13 +1,20 @@
 import os
 from flask import Flask
+from iatilib import Session, model
 
 app = Flask(__name__)
+# TODO update this at some point
+DEBUG = True
 
 @app.route('/')
 def hello():
-  return 'API Status: Healthy'
+    q = Session.query(model.LogEntry)
+    def row(x):
+        return '<tr><td>%s</td><td>%s</td><td>%s</td></tr>' % (x.id,x.timestamp.isoformat(),x.text)
+    table = '<table border="2">%s</table>' % ''.join( [row(x) for x in q] )
+    return '<html>API Status: Healthy<br/>%s</html>' % table
 
 if __name__=='__main__':
   port = int(os.environ.get('PORT',5000))
-  app.run(host='0.0.0.0',port=port)
+  app.run(host='0.0.0.0',port=port,debug=DEBUG)
 
