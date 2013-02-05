fb7abd09a796cb8d0d10387c9167e0f1e8b1545d
Tom Rees <zephod@gmail.com>
2013-02-05 19:49:30 +0000

    More robust parsing

--- iatilib/parser.py
+++ iatilib/parser.py
@@ -19,23 +19,20 @@ def parse(url):
         try:
             # Parse first...
             activity = model.Activity._parse_xml(logger, xml)
-            transactions   = [ model.Transaction._parse_xml(logger,x) for x in xml.findall('transaction') ]
-            sectors        = [ model.Sector._parse_xml(logger,x) for x in xml.findall('sector') ]
-            activitydates  = [ model.ActivityDate._parse_xml(logger,x) for x in xml.findall('activity-date') ]
-            contactinfos   = [ model.ContactInfo._parse_xml(logger,x) for x in xml.findall('contact-info') ]
-            participatings = [ model.ParticipatingOrg._parse_xml(logger,x) for x in xml.findall('participating-org') ]
+            for x in xml.findall('transaction'):
+                activity.transaction.append( model.Transaction._parse_xml(logger,x) )
+            for x in xml.findall('sector'):
+                activity.sector.append( model.Sector._parse_xml(logger,x) )
+            for x in xml.findall('activity-date'):
+                activity.activitydate.append( model.ActivityDate._parse_xml(logger,x) )
+            for x in xml.findall('contact-info'):
+                activity.contactinfo.append( model.ContactInfo._parse_xml(logger,x) )
+            for x in xml.findall('participating-org'):
+                activity.participatingorg.append( model.ParticipatingOrg._parse_xml(logger,x) )
             # ...then validate ...
-            _validate(activity,transactions,sectors,activitydates,contactinfos,participatings)
-            # ...then foreignkey ...
-            for x in transactions: activity.transaction.append(x)
-            for x in sectors: activity.sector.append(x)
-            for x in activitydates: activity.activitydate.append(x)
-            for x in contactinfos: activity.contactinfo.append(x)
-            for x in participatings: activity.participatingorg.append(x)
+            _validate(logger,activity)
             # .. then update the global object set
             activities.append(activity)
-        except ParseError as e:
-            logger.log(str(e))
         except (ValueError,AssertionError) as e:
             logger.log(str(e),level='error')
     return activities
@@ -43,29 +40,31 @@ def parse(url):
 # =========
 # Utilities
 # =========
-class ParseError(Exception):
-    def __init__(self, value):
-        self.value = value
-    def __str__(self):
-        return repr(self.value)
-
 class Logger():
     def __init__(self, prefix):
         self.prefix = prefix
     def log(self,string,level='warning'):
         log(level,self.prefix+string)
 
-def _parse_float(x):
+def _parse_float(logger,x):
     if x is None or x.strip()=='': return 0.0
     x = x.replace(',','')
-    return float(x)
+    try:
+        return float(x)
+    except ValueError, e:
+        logger.log(e)
+        return 0
 
-def _parse_int(x):
+def _parse_int(logger,x):
     if x is None or x.strip()=='': return 0
     x = x.replace(',','')
-    return int(x)
+    try:
+        return int(x)
+    except ValueError, e:
+        logger.log(e)
+        return 0
 
-def _parse_datetime(x):
+def _parse_datetime(logger,x):
     if x is None or x.strip()=='': return None
     try:
         if not ':' in x:
@@ -74,14 +73,16 @@ def _parse_datetime(x):
             x += '00:00:00'
         return iso8601.parse_date(x)
     except:
-        raise ParseError('Could not parse ISO date "%s"' % x)
+        logger.log('Could not parse ISO date "%s"' % x)
+        return iso8601.parse_date('2001-01-01Z00:00:00')
 
-def _parse_boolean(x):
+def _parse_boolean(logger,x):
     if x is None or x.strip()=='': return False
     x = x.lower().strip()
     if x=='true': return True
     if x=='false': return False
-    raise ParseError('Expected boolean True/False; got %s' % x)
+    logger.log('Expected boolean True/False; got %s' % x)
+    return False
 
 def _nav(logger, element, path, attrib=None, text=False, parser=None):
     assert text==True or (attrib is not None)
@@ -99,14 +100,14 @@ def _nav(logger, element, path, attrib=None, text=False, parser=None):
     if out is not None:
         out = unicode(out).strip()
     if parser is not None:
-        out = parser(out)
+        out = parser(logger,out)
     return out
 
-def _validate(activity,transactions,sectors,activitydates,contactinfos,participatings):
+def _validate(logger,activity):
     valid_sectors = set([ x.code for x in session.query(model.CodelistSector) ])
     assert len(valid_sectors), 'Found no CodelistSector'
-    for sector in sectors:
+    for sector in activity.sector:
         if sector.code==0: continue
         if not (sector.code in valid_sectors):
-            raise ParseError('Invalid sector.code="%d" (text="%s")' % (sector.code,sector.text))
+            logger.log('Invalid sector.code="%d" (text="%s")' % (sector.code,sector.text))
 
