a408a8d49cc14fb71ca5de7bac90299aa6c54f3e
Tom Rees <zephod@gmail.com>
2013-02-05 21:37:58 +0000

    Parallelised the downlaod script.

--- job_download_xml.py
+++ job_download_xml.py
@@ -9,48 +9,51 @@ import argparse
 import sys
 import json
 
-def download(debug_limit=None,verbose=False):
-    q = session.query(IndexedResource)\
-            .filter(IndexedResource.state<=0)\
-            .filter(IndexedResource.state!=-3)\
-            .limit(debug_limit)
-    count_resources = q.count()
-    if verbose:
-        print 'Found %d resources to download.' % count_resources
-    i = 0
-    for indexed_resource in q:
-        i += 1
-        state_strings = {
-                -1: 'new file',
-                -2: 'updated' }
-        state_string = state_strings.get(indexed_resource.state,'unknown')
-        url = indexed_resource.url
-        try:
-            if verbose:
-                print 'Scrape [%d/%d] (%s) %s... ' % (i,count_resources,state_string,url),
-                sys.stdout.flush()
-            # Download and import
-            activities,logerrors = parser.parse(url)
-            print 'Done. Got:', _object_summary(activities)
-            # Delete this resource's activities
-            for activity in indexed_resource.activities:
-                session.delete(activity)
-            for error in indexed_resource.logerrors:
-                session.delete(error)
-            session.commit()
-            # Add the new objects
-            for activity in activities:
-                indexed_resource.activities.append( activity )
-            for error in logerrors:
-                indexed_resource.logerrors.append( error )
-            indexed_resource.state=1
-            session.commit()
-        except IOError, e:
-            print 'Failed'
-            print '  > Could not download %s: %s' % (url,str(e))
-        except Exception as e:
-            traceback.print_exc()
-            print 'Uncaught error in: %s - %s' % (url, str(e))
+def download_loop(debug_limit=None,verbose=False):
+    downloaded = 0
+    while True:
+        q = session.query(IndexedResource)\
+                .filter(IndexedResource.state.in_([-1,-2]))
+        if verbose: 
+            print '%d resources in the queue to be downloaded.' % q.count()
+        indexed_resource = q.first()
+        if indexed_resource is None:
+            return
+        indexed_resource.state=-4
+        session.commit()
+        download(indexed_resource,verbose=verbose)
+        downloaded += 1
+        if (debug_limit is not None) and downloaded >= debug_limit:
+            return
+
+def download(indexed_resource,verbose=False):
+    url = indexed_resource.url
+    try:
+        if verbose:
+            print '-> %s... ' % (url),
+            sys.stdout.flush()
+        # Download and import
+        activities,logerrors = parser.parse(url)
+        print 'Done. Got:', _object_summary(activities)
+        # Delete this resource's activities
+        for activity in indexed_resource.activities:
+            session.delete(activity)
+        for error in indexed_resource.logerrors:
+            session.delete(error)
+        session.commit()
+        # Add the new objects
+        for activity in activities:
+            indexed_resource.activities.append( activity )
+        for error in logerrors:
+            indexed_resource.logerrors.append( error )
+        indexed_resource.state=1
+        session.commit()
+    except IOError, e:
+        print 'Failed'
+        print '  > Could not download %s: %s' % (url,str(e))
+    except Exception as e:
+        traceback.print_exc()
+        print 'Uncaught error in: %s - %s' % (url, str(e))
 
 def _object_summary(objects):
     if objects==[]:
@@ -67,5 +70,5 @@ if __name__=='__main__':
     argparser.add_argument('-d', '--debug', type=int, dest='debug_limit', help='Debug: Limit the number of records to be handled in this sweep.')
     argparser.add_argument('-v', '--verbose', action='store_true', help='Verbose mode')
     arg = argparser.parse_args()
-    download(debug_limit=arg.debug_limit,verbose=arg.verbose)
+    download_loop(debug_limit=arg.debug_limit,verbose=arg.verbose)
 
