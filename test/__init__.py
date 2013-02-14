455cc5be7dec7eb284111766f4965f603b809bae
Tom Dunham <tom@tomdunham.org>
2013-02-14 16:41:11 +0000

    Moved ClientTestCase

--- test/__init__.py
+++ test/__init__.py
@@ -3,11 +3,19 @@ import unittest
 from iatilib.frontend import create_app
 from iatilib import db
 
+
 def create_db():
     db.create_all()
+
 
 class AppTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app(
             SQLALCHEMY_DATABASE_URI='sqlite:///:memory:')
         create_db()
+
+
+class ClientTestCase(AppTestCase):
+    def setUp(self):
+        super(ClientTestCase, self).setUp()
+        self.client = self.app.test_client()
