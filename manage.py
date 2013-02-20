0ba7e4ca49b27bd6f89e4927e4b813355e94917a
Tom Dunham <tom@tomdunham.org>
2013-02-20 16:03:58 +0000

    Added parse_blob tool to help debugging.

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ from flask.ext.script import Manager
 
 
 from iatilib.frontend import create_app, db
-from iatilib import magic_numbers
+from iatilib import magic_numbers, parser
 from iatilib.model import IndexedResource, RawXmlBlob
 
 manager = Manager(create_app())
@@ -36,6 +36,25 @@ def status():
     parse_status = db.session.query(
         RawXmlBlob.parsed, sa.func.count()).group_by(RawXmlBlob.parsed)
     print qtable(parse_status, ["status", "no of blobs"])
+
+
+@manager.command
+def parse_blob(blob_id, pdb=False):
+    """
+    Parse a blob by id, don't insert into the db (debugging tool)
+    """
+    blob = RawXmlBlob.query.get(blob_id)
+    print "Blob %s parsed status=%r" % (blob.id, blob.parsed)
+    if pdb:
+        try:
+            import ipdb; ipdb.set_trace()
+        except ImportError:
+            import pdb; pdb.set_trace()
+    activity, errors = parser.parse(blob.raw_xml)
+    print "activity", activity
+    print "activity.id", activity.id
+    print "errors", errors
+
 
 
 
