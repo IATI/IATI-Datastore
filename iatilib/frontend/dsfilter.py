43c6e7d20e38a07cd6e23e8133821e877635e1e8
Tom Dunham <tom@tomdunham.org>
2013-03-11 12:02:05 +0000

    Filter tests pass

--- iatilib/frontend/dsfilter.py
+++ iatilib/frontend/dsfilter.py
@@ -1,41 +1,20 @@
-from sqlalchemy import func
-
+from iatilib import codelists
 from iatilib.model import Activity, CountryPercentage
-from iatilib import db
 
 
 def activities(args):
     query = Activity.query
 
+    if "country_code" in args:
+        country = codelists.Country.from_string(args["country_code"])
+        query = query.filter(
+            Activity.recipient_country_percentages.any(
+                CountryPercentage.country == country
+            )
+        )
 
-    filter_fields = {
-        u"country": (Activity.recipientcountry, RecipientCountry.text),
-        u"reporting_org_ref": (Activity.reportingorg, ReportingOrg.ref),
-    }
-
-    for arg_field, (table, table_field) in filter_fields.items():
-        if arg_field in args:
-            query = query.filter(
-                table.any(
-                    func.lower(table_field) == args[arg_field].lower())
-                )
-
-    # there are 4 dates: start-actual, end-actual, start-planned, end-planned
-    if "date" in args:
-        start_sq = db.session.query(
-            ActivityDate.parent_id,
-            func.min(ActivityDate.iso_date)
-            )\
-            .filter(ActivityDate.iso_date >= args["date"])\
-            .group_by(ActivityDate.parent_id).subquery()
-        end_sq = db.session.query(
-            ActivityDate.parent_id,
-            func.max(ActivityDate.iso_date)
-            )\
-            .filter(ActivityDate.iso_date <= args["date"])\
-            .group_by(ActivityDate.parent_id).subquery()
-        query = query\
-            .join(start_sq, Activity.id == start_sq.c.parent_id)\
-            .join(end_sq, Activity.id == end_sq.c.parent_id)\
+    if "reporting_org_ref" in args:
+        query = query.filter(
+            Activity.reporting_org_ref == args["reporting_org_ref"])
 
     return query
--- test/test_filter.py
+++ test/test_filter.py
@@ -7,7 +7,7 @@ from .factories import create_activity
 from iatilib import codelists as cl
 from iatilib.frontend import dsfilter
 
-@skip("filter tests")
+
 class TestFilter(AppTestCase):
     @skip("search by country name nonimpl")
     def test_by_country_name(self):
@@ -36,25 +36,12 @@ class TestFilter(AppTestCase):
         self.assertNotIn(act_not, activities.all())
 
     def test_by_reporting_org_ref(self):
-        act_in = create_activity(reporting_org__ref=u"AAA")
-        act_not = create_activity(reporting_org__ref=u"ZZZ")
+        act_in = fac.ActivityFactory.create(reporting_org__ref=u"AAA")
+        act_not = fac.ActivityFactory.create(reporting_org__ref=u"ZZZ")
         activities = dsfilter.activities({
             "reporting_org_ref": u"AAA"
         })
         self.assertIn(act_in, activities.all())
         self.assertNotIn(act_not, activities.all())
 
-    def test_date(self):
-        act_in = create_activity(
-            start_planned=datetime.date(2009, 1, 1),
-            end_planned=datetime.date(2009, 12, 31)
-        )
-        act_not = create_activity(
-            start_planned=datetime.date(2020, 1, 1),
-            end_planned=datetime.date(2020, 12, 31)
-        )
-        activities = dsfilter.activities({
-            "date": u"2009-05-09"
-        })
-        self.assertIn(act_in, activities.all())
-        self.assertNotIn(act_not, activities.all())
+
