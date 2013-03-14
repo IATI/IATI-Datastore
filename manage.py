ba28cb6f0122255eedadd4eb1e4515f451ee56ba
Tom Dunham <tom@tomdunham.org>
2013-03-14 22:04:15 +0000

    Cruft removal & queue command

--- manage.py
+++ manage.py
@@ -1,22 +1,33 @@
 import os
-import rq
-import urlparse
 import codecs
 import logging
 
 import requests
-import sqlalchemy as sa
-import prettytable
 from flask.ext.script import Manager
 from flask.ext.rq import get_worker
 
 from iatilib.frontend import create_app
-from iatilib import magic_numbers, parse, codelists, model, db, redis
+from iatilib import parse, codelists, model, db, redis
 from iatilib.crawler import manager as crawler_manager
+
+q_manager = Manager(usage="Background task queue")
+
+
+@q_manager.command
+def burst():
+    "Run jobs then exit when queue is empty"
+    get_worker().work(burst=True)
+
+
+@q_manager.command
+def background():
+    "Monitor queue for jobs and run when they are there"
+    get_worker().work(burst=False)
 
 
 manager = Manager(create_app(DEBUG=True))
 manager.add_command("crawl", crawler_manager)
+manager.add_command("queue", q_manager)
 
 
 @manager.shell
@@ -27,113 +38,6 @@ def make_shell_context():
         rdb=redis,
         models=model,
         codelists=codelists)
-
-
-def qtable(itr, headers=None):
-    if headers is None:
-        headers = next(itr)
-    tbl = prettytable.PrettyTable(headers)
-    for row in itr:
-        tbl.add_row(row)
-    return tbl
-
-
-@manager.command
-def status():
-    print "Database: %s" % manager.app.config["SQLALCHEMY_DATABASE_URI"]
-    print
-
-    resource_status = db.session.query(
-        IndexedResource.state, sa.func.count()).group_by(IndexedResource.state)
-
-    print "Download"
-    print qtable(
-        ((magic_numbers.label[n], c) for n, c in resource_status),
-        ["state", "no of urls"])
-
-    print "Parsing"
-    parse_status = db.session.query(
-        RawXmlBlob.parsed, sa.func.count()).group_by(RawXmlBlob.parsed)
-    print qtable(parse_status, ["status", "no of blobs"])
-
-
-@manager.command
-def parse_blob(blob_id, pdb=False):
-    """
-    Parse a blob by id, don't insert into the db (debugging tool)
-    """
-    blob = RawXmlBlob.query.get(blob_id)
-    print "Blob %s parsed status=%r" % (blob.id, blob.parsed)
-    if pdb:
-        try:
-            import ipdb
-            ipdb.set_trace()
-        except ImportError:
-            import pdb
-            pdb.set_trace()
-    activity, errors = parser.parse(blob.raw_xml)
-    print "activity", activity
-    print "activity.id", activity.id
-    print "errors", errors
-
-
-@manager.command
-def dump_blob(blob_id):
-    blob = RawXmlBlob.query.get(blob_id)
-    print blob.raw_xml
-
-
-def redis_connect():
-    redis_url = os.getenv('REDISTOGO_URL', "redis://localhost:6379")
-    urlparse.uses_netloc.append('redis')
-    url = urlparse.urlparse(redis_url)
-    return redis.Redis(
-        host=url.hostname,
-        port=url.port,
-        db=0,
-        password=url.password)
-
-
-@manager.command
-def bulk_parse(max_blobs=None):
-    "Enque jobs to parse all unparsed blobs"
-    if max_blobs:
-        max_blobs = int(max_blobs)
-    q = rq.Queue(connection=redis_connect())
-    query = RawXmlBlob.query.filter_by(parsed=False)
-    print "There are %d blobs that need to be parsed" % query.count()
-    if not max_blobs:
-        max_blobs = query.count()
-    print "Enquing upto %d blobs" % max_blobs
-    for blob in query.limit(max_blobs):
-        q.enqueue_call(
-            func=parser.parse_job,
-            args=(blob.id,),
-            result_ttl=0)
-
-
-@manager.command
-def bulk_parse_failiures():
-    "Print the blob ids that failed to parse"
-    failed_q = rq.Queue("failed", connection=redis_connect())
-    job = failed_q.dequeue()
-    while job:
-        raw_xml_blob_id = job.args[0]
-        print raw_xml_blob_id
-        job = failed_q.dequeue()
-
-
-@manager.command
-def rqworker(burst=False):
-    "Start an RQ worker"
-    with rq.Connection(redis_connect()):
-        worker = rq.Worker(rq.Queue())
-        worker.work(burst=burst)
-
-
-@manager.command
-def batch_worker():
-    get_worker().work(burst=True)
 
 
 @manager.command
@@ -173,6 +77,7 @@ def parse_file(filenames, verbose=False, fail_xml=False, fail_spec=False):
                 raise
             if isinstance(exc, parse.SpecError) and fail_spec:
                 raise
+
 
 @manager.command
 def create_database():
