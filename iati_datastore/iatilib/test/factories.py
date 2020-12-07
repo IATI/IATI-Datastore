f0a4ae96b878f839462a47cd4e66c667e25ae423
Mark Brough <mark.brough@gmail.com>
2020-12-07 15:37:33 +0100

    Update factory setup from new versions of factory boy and sqlalchemy

--- iati_datastore/iatilib/test/factories.py
+++ iati_datastore/iatilib/test/factories.py
@@ -3,7 +3,7 @@ import datetime
 import factory
 
 from iatilib import codelists
-from iatilib.frontend import db
+from iatilib import db
 from iatilib.model import (
     Activity, Transaction, Organisation, SectorPercentage, CountryPercentage,
     Participation, Budget, Resource, RegionPercentage, PolicyMarker,
@@ -11,62 +11,89 @@ from iatilib.model import (
 )
 
 
-def sa_creation(model, **kw):
-    instance = model(**kw)
-    db.session.add(instance)
-    db.session.commit()
-    db.session.refresh(instance)
-    return instance
 
-factory.Factory.set_creation_function(sa_creation)
+from factory.alchemy import SQLAlchemyModelFactory as Factory
+
+class TestFactory(Factory):
+    class Meta:
+        abstract = False
+        sqlalchemy_session = db.session
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        entity = model_class(*args, **kwargs)
+        db.session.add(entity)
+        db.session.commit()
+        db.session.refresh(entity)
+        return entity
 
 
 def create_activity():
     raise Exception("create_activity")
 
 
-class ResourceFactory(factory.Factory):
+class ResourceFactory(TestFactory):
+    class Meta:
+        model = Resource
     url = u"http://test.com"
 
 
-class CountryPercentageFactory(factory.Factory):
+class CountryPercentageFactory(TestFactory):
+    class Meta:
+        model = CountryPercentage
     country = codelists.Country.united_states
     name = ""
     percentage = 0
 
 
-class RegionPercentageFactory(factory.Factory):
+class RegionPercentageFactory(TestFactory):
+    class Meta:
+        model = RegionPercentage
     region = codelists.Region.europe_regional
     percentage = 0
 
 
-class SectorPercentageFactory(factory.Factory):
+class SectorPercentageFactory(TestFactory):
+    class Meta:
+        model = SectorPercentage
     sector = codelists.Sector.teacher_training
     percentage = 0
 
 
-class OrganisationFactory(factory.Factory):
+class OrganisationFactory(TestFactory):
+    class Meta:
+        model = Organisation
     ref = factory.Sequence(lambda n: u'test-org-{0}'.format(n))
     name = u"test org"
 
 
-class ParticipationFactory(factory.Factory):
+class ParticipationFactory(TestFactory):
+    class Meta:
+        model = Participation
     organisation = factory.SubFactory(OrganisationFactory)
     role = codelists.OrganisationRole.funding
 
-class PolicyMarkerFactory(factory.Factory):
+class PolicyMarkerFactory(TestFactory):
+    class Meta:
+        model = PolicyMarker
     code = codelists.PolicyMarker.gender_equality
 
-class RelatedActivityFactory(factory.Factory):
+class RelatedActivityFactory(TestFactory):
+    class Meta:
+        model = RelatedActivity
     ref = "test_ref"
 
-class ActivityFactory(factory.Factory):
+class ActivityFactory(TestFactory):
+    class Meta:
+        model = Activity
     iati_identifier = factory.Sequence(lambda n: u'test-act-{0}'.format(n))
     raw_xml = u"<test />"
     major_version = '1'
 
 
-class TransactionFactory(factory.Factory):
+class TransactionFactory(TestFactory):
+    class Meta:
+        model = Transaction
     activity = factory.SubFactory(ActivityFactory)
     type = codelists.TransactionType.commitment
     value_currency = codelists.Currency.us_dollar
@@ -74,12 +101,16 @@ class TransactionFactory(factory.Factory):
     value_date = datetime.date(1972, 1, 1)
     date = datetime.date(1973, 2, 2)
 
-class BudgetFactory(factory.Factory):
+class BudgetFactory(TestFactory):
+    class Meta:
+        model = Budget
     activity = factory.SubFactory(ActivityFactory)
     type = codelists.BudgetType.original
     value_amount = 0
 
-class DatasetFactory(factory.Factory):
+class DatasetFactory(TestFactory):
+    class Meta:
+        model = Dataset
     name = 'test_dataset'
     resources = factory.SubFactory(ResourceFactory)
     is_open = True
